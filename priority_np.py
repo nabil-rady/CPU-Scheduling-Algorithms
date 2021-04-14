82ffbd125aceeae0eaa06340f48a177eae6897d9
My-Name-Is-Nabil <midorady9999@gmail.com>
2021-04-14 23:23:30 +0200

    Changed naming conventions

--- priority_np.py
+++ priority_np.py
@@ -1,38 +1,29 @@
-# global variables
-finishedProcesses = [0]
-gantChart = [0]
-
-
-def handel_priority(processes):
-    # using global variables
-    global finishedProcesses
-    global gantChart
-    
+def handle_priority(processes,finished_processes,gantt_chart):
     # get the last executed process
     prev = {
         'name': processes[0]['name'],
         'priority': processes[0]['priority'],
         'burst_time': processes[0]['burst_time'],
-        'const_arrival': processes[0]['const_arrival']
+        'const_arrival_time': processes[0]['const_arrival_time']
     }
 
-    finishedProcesses.append(prev)
+    finished_processes.append(prev)
     processes.pop(0)
     
-    # to calculate the waiting time and complete the gantChart
-    finishedProcesses[0] += gantChart[len(gantChart) - 1] - prev['const_arrival']
-    time = gantChart[len(gantChart) - 1] + prev['burst_time']
-    gantChart.append(time)
+    # to calculate the waiting time and complete the gantt_chart
+    finished_processes[0] += gantt_chart[len(gantt_chart) - 1] - prev['const_arrival_time']
+    time = gantt_chart[len(gantt_chart) - 1] + prev['burst_time']
+    gantt_chart.append(time)
 
-    # handel the different arrival times
+    # handle the different arrival times
     for i in range(len(processes)):
-        if processes[i]['arrival'] > 0:
-            if prev['burst_time'] >= processes[i]['arrival']:
-                processes[i]['arrival'] = 0
+        if processes[i]['arrival_time'] > 0:
+            if prev['burst_time'] >= processes[i]['arrival_time']:
+                processes[i]['arrival_time'] = 0
             else:
-                processes[i]['arrival'] -= prev['burst_time']
+                processes[i]['arrival_time'] -= prev['burst_time']
 
-    processes = sorted(processes, key=lambda k: (k['arrival'], k['const_arrival'], k['priority'], k['burst_time']))
+    processes = sorted(processes, key=lambda k: (k['arrival_time'], k['const_arrival_time'], k['priority'], k['burst_time']))
     return processes
 
 
@@ -46,26 +37,26 @@ def handel_priority(processes):
 # processes = [{}, {}, ......]
 
 # the return array contains average waiting at index 0,
-# the gantChart times at the last elemet
+# the gantt_chart times at the last elemet
 
 def priority_np(processes):
     # using global variables
-    global finishedProcesses
-    global gantChart
+    finished_processes = [0]
+    gantt_chart = [0]
     
     # make sure that the processes is sorted base on their priority
-    processes = sorted(processes, key=lambda k: (k['arrival'], k['priority'], k['burst_time']))
+    processes = sorted(processes, key=lambda k: (k['arrival_time'], k['priority'], k['burst_time']))
     
     n = len(processes)
     i = n
     
     while i > 0:
-        processes = handel_priority(processes)
+        processes = handle_priority(processes,finished_processes,gantt_chart)
         i-=1
     
-    finishedProcesses[0] /=  n
-    finishedProcesses.append(gantChart)
+    finished_processes[0] /=  n
+    finished_processes.append(gantt_chart)
 
-    return finishedProcesses
+    return finished_processes
 
 
--- roundrobin.py
+++ roundrobin.py
@@ -1,32 +1,22 @@
 import numpy as np
 
-def is_sorted(arr):
-    last = arr[0]
-    for i in range(1,len(arr)):
-        if arr[i]<last:
-            return False
-        last = arr[i]
-    return True
-
 # Function to check if all processes are done
-def prcoesses_done(processes):
-    for process in processes:
-        if process>0:
+def prcoesses_done(burst_times):
+    for burst_time in burst_times:
+        if burst_time>0:
             return False
     return True
 
 
-def roundrobin(burst_times,time_quantum,*,arrival_times=None):
+def roundrobin(processes,time_quantum):
+    burst_times = [process['burst_time'] for process in processes]
+    arrival_times = [process['arrival_time'] for process in processes]
     number_of_processes = len(burst_times)    
-    if arrival_times is None:
-        arrival_times = [0]*number_of_processes 
-    if len(burst_times)!=len(arrival_times):
-        raise Exception('Invalid Input')
     current_time = min(arrival_times)
     counter = 0
     burst_times = burst_times.copy()
     x_ticks = [current_time]    # This variable represents ticks on the  X-axis
-    processes = list()  # This variable represents the process on the respective X-axis tick
+    processes_names = list()  # This variable represents the process on the respective X-axis tick
     waiting_time = [0]*number_of_processes  # Waiting time for each process
     last_time_checked = arrival_times.copy()    # Last time the process was active or the time of arrival
     while not prcoesses_done(burst_times):
@@ -39,15 +29,15 @@ def roundrobin(burst_times,time_quantum,*,arrival_times=None):
                     waiting_time[counter]+=current_time-last_time_checked[counter]
                     current_time+=time_quantum
                     burst_times[counter]-=time_quantum
-                    processes.append(counter+1)
+                    processes_names.append(processes[counter]['name'])
                     last_time_checked[counter]=current_time
                 else:
                     x_ticks.append(current_time+burst_times[counter])
                     waiting_time[counter]+=current_time-last_time_checked[counter]
                     current_time+=burst_times[counter]
                     burst_times[counter]=0
-                    processes.append(counter+1)
+                    processes_names.append(processes[counter]['name'])
                     last_time_checked[counter]=current_time
         counter = (counter+1)%number_of_processes
         average_waiting_time = sum(waiting_time)/len(waiting_time)
-    return average_waiting_time,np.asarray(processes),np.asarray(x_ticks)
+    return average_waiting_time,np.asarray(processes_names),np.asarray(x_ticks)
